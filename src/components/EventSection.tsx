b38890fa575cce3889691cca4916a3f1e9444ed7
Naveen <imnaveenbharath@gmail.com>
2023-10-26 09:55:02 +0530

    hero and navbar

---
+++ src/components/EventSection.tsx
@@ -0,0 +1,6 @@
+import Container from "./Container";
+
+function EventSection() {
+  return <section>{/* <Container></Container> */}</section>;
+}
+export default EventSection;
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,15 @@
+import Container from "./Container";
+
+function HeroSection() {
+  return (
+    <section
+      id="hero"
+      className="relative flex h-[70vh] w-full items-center justify-center bg-[url(/bg.jpg)] bg-cover bg-no-repeat pt-20"
+    >
+      <Container>
+        <span className="text-6xl font-extrabold text-white">Niral Thiral</span>
+      </Container>
+    </section>
+  );
+}
+export default HeroSection;
